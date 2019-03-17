f707474fdf47ce5b63e37786d1a9de508e67488e
Lee Katz <lee@minutemedia.com>
2019-03-17 11:40:58 +0200

    start influx

--- src/plugins/influxDbWriter.js
+++ src/plugins/influxDbWriter.js
@@ -1,3 +1,19 @@
+import { InfluxDB } from 'influx';
+
 export function runInfluxDbPlugin() {
   console.log('running influxDb plugin');
+  const influx = new InfluxDB({
+    host: 'localhost',
+    database: 'lighthouse',
+    port: 8086,
+  });
+  influx.writePoints([
+    {
+      measurement: 'stats',
+      tags: 'tags',
+      fields: {},
+    }])
+    .then(() => {
+      console.log('Added data to the lighthouse influxDB');
+    });
 }
