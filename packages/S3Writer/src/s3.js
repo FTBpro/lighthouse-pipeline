f78f83ab01c6c9ecc7fe2b36f8e710daace35a47
Erez <luoruize@gmail.com>
2019-03-18 12:06:13 +0200

    Import from core (#15)
    * import from core
    
    * lighthouse and s3 connection

--- packages/S3Writer/src/s3.js
+++ packages/S3Writer/src/s3.js
@@ -16,19 +16,21 @@ function uploadObject(s3, data, key, contentType) {
 
 /**
  * 
- * @param {{credentials: string}} config credentials: path to the file containing AWS credentials
- * @param {{path: string, data: string, type: "json | html"}} data 
+ * @param {{path: string}} config {path: path to the file containing AWS credentials}
+ * @param {{lhr: {}, report: {}}} data the data returned from lighthouse
  */
 export function runS3Plugin(config, data) {
   const { path } = config;
+  console.log('S3 report:', data.report);
+  console.log('typeof:', typeof data.report);
   AWS.config.loadFromPath(config.credentials);
 
   const s3 = new AWS.S3();
 
   const now = (new Date()).toISOString();
-
+  console.log('writing to S3...');
   return Promise.all([
     uploadObject(s3, JSON.stringify(data.lhr), `${path}/${now}/lighthouse.json`, 'application/json'),
-    uploadObject(s3, JSON.stringify(data.report), `${path}/${now}/lighthouse.html`, 'text/html'),
+    // uploadObject(s3, data.report, `${path}/${now}/lighthouse.html`, 'text/html'),
   ]);
 }
