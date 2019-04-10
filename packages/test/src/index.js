02f46f5b7f87f68720f4657378227e7cd5515d3e
Lee Katz <lee@minutemedia.com>
2019-04-10 18:10:23 +0300

    add package lock

--- packages/test/src/index.js
+++ packages/test/src/index.js
@@ -5,37 +5,65 @@ import { runS3Plugin } from '@lighthouse-pipeline/s3-writer-plugin';
 const path = require('path');
 const app = require('express')();
 
+const jobs = [];
+
 const port = 1337;
 
-app.get('/', (req, res) => {
+app.get('/add', (req, res) => {
   const { url, tag } = req.query;
   if (!url) {
-    return res.status(500).send('URL not defined');
+    return res.status(400).send('URL not defined');
   }
+  if (!tag) {
+    return res.status(400).send('Tag not defined');
+  }
+  addToJobs(url, tag);
+  res.sendStatus(204);
+});
 
-  console.log('running pipeline');
+const addToJobs = (url, tag) => {
+  const job = { url, tag, timestamp: Date.now() };
+  jobs.push(job);
+  console.log('added job: ', job);
+};
 
-  runPipeline()
-    .registerTag(tag)
+
+function runLighthouseAudit(url, tag) {
+  return runPipeline()
     .registerUrl(url)
-    .registerPlugin(runS3Plugin, { credentials: path.join(__dirname, '../.env'), path: 'test', bucket: 'minutemedia-lighthouse' })
+    .registerChromePath('/usr/bin/chromium-browser')
+    .registerLighthouseOptions({
+      blockedUrlPatterns: ['facebook.com/tr'],
+    })
+    .registerPlugin(runS3Plugin, {
+      credentials: path.join(__dirname, './.env'),
+      path: 'test',
+      bucket: 'minutemedia-lighthouse'
+    })
     .registerPlugin(runInfluxDbPlugin, {
-      host: 'localhost',
+      host: '10.0.15.132',
       database: 'lighthouse',
       port: 8086,
     })
     .run()
-    .then((response) => {
-      console.log(response);
-      if (response.length >= 2) {
-        const [html, json] = response[0];
-        res.send(`Done:<br /><a href="${html.Location}">HTML</a><br /><a href="${json.Location}">JSON</a>`);
-      } else {
-        res.send('All done, boy!');
-      }
-    });
-});
+    .catch(err => console.log('err', err));
+}
+
+const jobRunner = () => {
+  if (jobs.length !== 0) {
+    const job = jobs.pop();
+    console.log('running job: ', job);
+    runLighthouseAudit(job.url, job.tag)
+      .then(() => console.log('finished running job: ', job))
+      .catch(() => console.log('error while running job: ', job))
+      .finally(() => setTimeout(jobRunner, 10000));
+  }
+  else {
+    setTimeout(jobRunner, 10000);
+  }
+};
 
 app.listen(port, () => {
-  console.log(`Mock server running on ${port}`);
+  console.log(`Server running on ${port}`);
 });
+jobRunner();
